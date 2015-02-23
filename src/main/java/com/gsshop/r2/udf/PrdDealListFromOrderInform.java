a61a2b249313e759e4850621e22b53e6ea83a80a
baccas300 <baccas300@gmail.com>
2015-02-23 16:05:01 +0900

    주문장바구니 상품딜 정보 가져오는 펑션

---
+++ src/main/java/com/gsshop/r2/udf/PrdDealListFromOrderInform.java
@@ -0,0 +1,45 @@
+package com.gsshop.r2.udf;
+
+import org.apache.hadoop.io.Text;
+
+public class PrdDealListFromOrderInform {
+	public PrdDealListFromOrderInform(){
+		
+	}
+	public Text evaluate(Text contents, Text type, Text delim) {
+		String eventline = contents.toString();
+		String contenttype = type.toString();
+		String delimeter = delim.toString();
+		Text result=null;
+		String rst="";
+		if(contenttype.equals("ordprd")){
+			String[] ords = eventline.split("[|]");
+			
+			for(String aord : ords){
+				if(rst.equals("")) rst=aord.split(",")[0];
+				else rst=rst+delimeter+ aord.split(",")[0];
+			}
+		}else if(contenttype.equals("cartprd")){
+			String[] carts = eventline.split("[|]");
+			for(String acart : carts){
+				if(rst.equals("")) rst=acart.split(",")[0];
+				else rst=rst+delimeter+ acart.split(",")[0];
+			}
+		}else if(contenttype.equals("orddeal")){
+			String[] ords = eventline.split("[|]");
+			
+			for(String aord : ords){
+				if(rst.equals("")) rst=aord.split(",")[3];
+				else rst=rst+delimeter+ aord.split(",")[3];
+			}
+		}else if(contenttype.equals("cartdeal")){
+			String[] carts = eventline.split("[|]");
+			for(String acart : carts){
+				if(rst.equals("")) rst=acart.split(",")[1];
+				else rst=rst+delimeter+ acart.split(",")[1];
+			}
+		}
+		result=new Text(rst);
+		return result;
+	}
+}
